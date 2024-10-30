925467acc0b7f2d9dc3297e3857cac60478e2627
massimopallera <pallera.massimo@gmail.com>
2024-10-30 16:36:43 +0100

    update: create new rout for posts

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const host = 'http://127.0.0.1'
 const port = 3000
 
 app.get('/', (req,res) => {
-  // res.send("<h1>Benvenuto nel mio blog</h1>")
-  res.json(posts.createPosts())
+  res.send("<h1>Benvenuto nel mio blog</h1>")
+})
 
+app.get('/posts', (rq,res) => {
+  res.json(posts.createPosts())
 })
 
 app.listen(port,()=>{
